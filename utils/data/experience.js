4704ac69cf96ce65f7ab376717f8c3ce1920feeb
Roberto Mencia <roberto.mencia@softwareone.com>
2024-04-30 15:11:01 +0200

    meses

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -3,36 +3,36 @@ export const experiences = [
     id: 1,
     title: 'Senior Cloud Software Developer',
     company: "SoftwareONE",
-    duration: "(Mayo 2021 - Actualidad)"
+    duration: "(May 2021 - Present)"
   },
   {
     id: 2,
     title: "Analyst Senior Software Developer",
     company: "Proconsi",
-    duration: "(Junio 2018 - Mayo 2021)"
+    duration: "(Jun 2018 - May 2021)"
   },
   {
     id: 3,
     title: "Senior Solutions Analyst",
     company: "ICON Multimedia",
-    duration: "(Noviembre 2016 - Mayo 2018)"
+    duration: "(Nov 2016 - May 2018)"
   },
   {
     id: 4,
     title: "Solutions Specific Knowledge Analyst",
     company: "Everis",
-    duration: "(Abril 2015 - Junio 2016)"
+    duration: "(Apr 2015 - Jun 2016)"
   },
   {
     id: 5,
     title: "Senior Analyst",
     company: "KEYLAND SISTEMAS DE GESTION",
-    duration: "(Marzo 2014 - Marzo 2015)"
+    duration: "(March 2014 - March 2015)"
   },
   {
     id: 6,
     title: "Senior Analyst",
     company: "Avanade Spain S.L",
-    duration: "(Jul 2007 - Marzo 2014)"
+    duration: "(Jul 2007 - March 2014)"
   }
 ]
